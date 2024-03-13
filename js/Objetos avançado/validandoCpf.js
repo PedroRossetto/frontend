42c84efc95f1b7334270cb617eb2fbc786d03580
Pedro Arthur Rossetto <pedro.rossetto@LTUPAN.INTRANET>
2024-03-13 09:29:47 -0300

    Alteração

--- js/Objetos avançado/validandoCpf.js
+++ js/Objetos avançado/validandoCpf.js
@@ -1,4 +1,3 @@
-// CPF : 520.254.728-54
 //VALIDAÇÃO = (5*10) + (2*9) + (0*8) + (2*7) + (5*6) + (4*5) + (7*4) + (2*3) + (8*2)  = 182
 // (182*10)/11 = 5 RESTO DESSA DIVISÃO É A REPRESENTAÇÃO DO PRIMEIRO NUMERO DOS 2 ULTIMOS NUMEROS 
 // NO CASO SE FOR 10 CONSIDERA 0 ENTÃO 
@@ -10,7 +9,7 @@
 // (227*10)%11 =  4
 // console.log (((5*11) + (2*10) + (0*9) + (2*8) + (5*7) + (4*6) + (7*5) + (2*4) + (8*3) + (5*2))*10)
 
-let cpfInserido = "52025472854" , soma = 0, contador = 0, verificador = 0;
+let cpfInserido = "32028452821" , soma = 0, contador = 0, verificador = 0;
 
     for (let i = 10; i > 1; i--) {
         soma += (parseInt(cpfInserido[contador]))*i;
